b7603eb448511437dde2e9038f79413134c4c5f3
Office <karavi@ntk.ir>
2020-11-30 17:26:31 +0330

    bug

--- cpanelv1/CmsModules/Application/ApplicationApp/ApplicationAppController.js
+++ cpanelv1/CmsModules/Application/ApplicationApp/ApplicationAppController.js
@@ -1023,7 +1023,7 @@
                 angular.forEach(response1.ListItems, function(value, key) {
                     node.Children.push(value);
                 });
-                ajax.call(cmsServerConfig.configApiServerPath + "FileContent/GetFilesInCategoryId/"+ node.Id,, 'GET').success(function(response2) {
+                ajax.call(cmsServerConfig.configApiServerPath + "FileContent/GetFilesInCategoryId/"+ node.Id,"", 'GET').success(function(response2) {
                     angular.forEach(response2.ListItems, function(value, key) {
                         node.Children.push(value);
                     });
